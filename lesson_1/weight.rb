8266cc2f77b7e97ce2d397451aeff6315a6bc0ae
legiorex <batyushev@gmail.com>
2021-01-26 21:27:30 +0300

    lesson_1

---
+++ lesson_1/weight.rb
@@ -0,0 +1,15 @@
+puts 'What is your name?'
+
+name = gets.chomp
+
+puts 'How height are you?'
+
+height = Integer(gets.chomp)
+
+weight = (height - 110) * 1.15
+
+if weight < 0
+    puts "#{name} you weight is good!"
+else
+    puts "#{name} you weight is no good!"
+end
