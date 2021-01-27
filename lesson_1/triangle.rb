8e020e0975f927d02080913a52b12c7d90d40c5d
legiorex <batyushev@gmail.com>
2021-01-27 11:20:26 +0300

    triangle

---
+++ lesson_1/triangle.rb
@@ -0,0 +1,10 @@
+puts 'Enter the base of the triangle'
+
+base_triangle = Integer(gets.chomp)
+
+puts 'Enter the height of the triangle'
+
+height_triangle = Integer(gets.chomp)
+
+square_triangle = 1.0 / 2.0 * base_triangle * height_triangle
+puts "The Square of ​​your triangle #{square_triangle}"
