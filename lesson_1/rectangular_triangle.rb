9e29bebccd22c20fdf0d665e70a7c18c1d9ddee0
legiorex <batyushev@gmail.com>
2021-01-27 16:56:13 +0300

    create rest triangle

---
+++ lesson_1/rectangular_triangle.rb
@@ -0,0 +1,29 @@
+
+puts 'Enter the first side of the triangle'
+triangle_a = gets.chomp.to_f
+
+puts 'Enter the second side of the triangle'
+triangle_b = gets.chomp.to_f
+
+puts 'Enter the third side of the triangle'
+triangle_c = gets.chomp.to_f
+
+triangle_current = [triangle_a, triangle_b, triangle_c]
+
+hypotenuse = triangle_current.max
+
+cathetus = triangle_current.reject { |value| value === hypotenuse }
+
+if cathetus.empty?
+    puts "equilateral triangle"
+elsif cathetus.size === 2
+        cathetus_a = cathetus[0]
+        cathetus_b = cathetus[1]
+        is_orthogonal_triangle = hypotenuse ** 2 === (cathetus_a ** 2 + cathetus_b ** 2)
+
+        isosceles_or_regular_triangle = cathetus_a === cathetus_b ? "isosceles triangle" : "regular triangle"
+
+        puts is_orthogonal_triangle ? "orthogonal triangle" : isosceles_or_regular_triangle
+else
+puts "isosceles triangle"
+end
--- lesson_1/triangle.rb
+++ lesson_1/triangle.rb
@@ -1,10 +1,10 @@
 puts 'Enter the base of the triangle'
 
-base_triangle = Integer(gets.chomp)
+base_triangle = gets.chomp.to_f
 
 puts 'Enter the height of the triangle'
 
-height_triangle = Integer(gets.chomp)
+height_triangle = gets.chomp.to_f
 
 square_triangle = 1.0 / 2.0 * base_triangle * height_triangle
 puts "The Square of ​​your triangle #{square_triangle}"
