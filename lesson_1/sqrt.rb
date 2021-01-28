4aba978bfd1fae85a8520475f52b31d186c07ad4
legiorex <batyushev@gmail.com>
2021-01-28 12:29:13 +0300

    fix comment reviewer

--- lesson_1/sqrt.rb
+++ lesson_1/sqrt.rb
@@ -13,8 +13,10 @@ puts "D #{d}"
 if d < 0
   puts "D = #{d} not sqrt"
 else
-  x_1 = (- b + Math.sqrt(d)) / 2 * a
-  x_2 = (- b - Math.sqrt(d)) / 2 * a
+  sqrt_d = d === 0 ? 0.0 : Math.sqrt(d)
+
+  x_1 = (- b + sqrt_d) / 2 * a
+  x_2 = (- b - sqrt_d) / 2 * a
 
   puts d === 0 ? "D = #{d} X = #{x_1}" : "D = #{d} X1 = #{x_1} X2 = #{x_2}"
 end
