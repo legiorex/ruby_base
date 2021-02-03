b1d866ffd216cfaeddf842578c98034c350707c5
legiorex <batyushev@gmail.com>
2021-02-03 17:41:52 +0300

    fix comment reveiwer date_count

--- lesson_2/date_count.rb
+++ lesson_2/date_count.rb
@@ -5,31 +5,19 @@ loop do
   if year.zero?
     puts 'You entered the wrong year format.'
     year = gets.chomp.to_i
-  elsif year.to_s.size != 4
+  elsif year.to_s.size > 4
     puts 'You entered the wrong year format.'
     year = gets.chomp.to_i
   end
-  break if year.to_s.size == 4 && year.to_i != 0
+  break if year.to_s.size <= 4 && year.to_i != 0
 end
 year = year.to_i
-is_leap_year = (year % 4).zero? || (year % 400).zero?
+
+is_leap_year = ((year % 4).zero? && !(year % 100).zero?) || (year % 400).zero?
 
 days_february = is_leap_year ? 29 : 28
 
-months = {
-  1 => 31,
-  2 => days_february,
-  3 => 31,
-  4 => 30,
-  5 => 31,
-  6 => 30,
-  7 => 31,
-  8 => 31,
-  9 => 30,
-  10 => 31,
-  11 => 30,
-  12 => 31
-}
+months = [31, days_february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 puts 'get month'
 current_month = gets.chomp.to_i
@@ -45,17 +33,15 @@ puts 'get day'
 current_day = gets.chomp.to_i
 
 loop do
-  unless (1..months[current_month]).include?(current_day)
+  unless (1..months[current_month - 1]).include?(current_day)
     puts 'You entered the wrong day in the selected month, please try again.'
     current_day = gets.chomp.to_i
   end
-  break if (1..months[current_month]).include?(current_day)
+  break if (1..months[current_month - 1]).include?(current_day)
 end
 
-count_date = 0
-months.each do |month, day|
-  count_date += day if month != current_month
-  count_date += current_day if month == current_month
+if current_month == 1
+  puts " count #{current_day}"
+else
+  puts " count #{months.take(current_month - 1).sum + current_day}"
 end
-
-puts " count #{count_date}"
