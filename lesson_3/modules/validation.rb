fa9045b68545bb75b33cca978d863c1c299dd813
legiorex <batyushev@gmail.com>
2021-03-23 11:20:29 +0300

    fix comment reviewer

--- lesson_3/modules/validation.rb
+++ lesson_3/modules/validation.rb
@@ -9,9 +9,9 @@ module Validation
       @validations || []
     end
 
-    def validate(name, type, *options, **hash)
+    def validate(name, type, *args, **options)
       @validations ||= []
-      @validations << { attr: name, type: type, options: options, message: hash }
+      @validations << { attr: name, type: type, args: args, options: options }
     end
   end
 
@@ -27,24 +27,24 @@ module Validation
       self.class.validations.each do |validation|
         value = instance_variable_get("@#{validation[:attr]}")
 
-        send("validate_#{validation[:type]}", value, *validation[:options], **validation[:message])
+        send("validate_#{validation[:type]}", validation[:attr], value, *validation[:args], **validation[:options])
       end
     end
 
-    def validate_presence(value, **message)
-      error_message = message[:message] || 'Значение не заполнено'
+    def validate_presence(attr, value, **options)
+      error_message = options[:message] || "Значение #{attr} не заполнено"
 
       raise error_message if value.nil? || value.empty?
     end
 
-    def validate_format(value, *options, **message)
-      error_message = message[:message] || 'number does not match the format'
-      raise error_message if value !~ options[0]
+    def validate_format(attr, value, *args, **options)
+      error_message = options[:message] || "#{attr} не должно быть пустым"
+      raise error_message if value !~ args[0]
     end
 
-    def validate_type(value, *options, **message)
-      error_message = message[:message] || 'invalid class type'
-      raise error_message unless value.is_a? Object.const_get(options[0].to_s)
+    def validate_type(attr, value, *args, **options)
+      error_message = options[:message] || "#{attr} invalid class type"
+      raise error_message unless value.is_a? Object.const_get(args[0].to_s)
     end
   end
 end
